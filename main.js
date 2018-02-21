461bba25135eddaa56dba6a1c7d3de707074843c
Ivan Prokopenko <web.prokopenko@gmail.com>
2018-02-21 12:24:51 +0200

    Webpack start

---
+++ main.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function(){
+   var sayHello = require('./sayHello').sayHello;
+   var hello = sayHello('Browser!');
+   var body = document.getElementsByTagName('body')[0];
+   body.innerHTML = hello;
+});
